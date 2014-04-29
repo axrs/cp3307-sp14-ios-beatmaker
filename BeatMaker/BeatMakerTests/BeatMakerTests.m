39fc42b3f3a470e1ad66dea5f9ed230cc4e34163
Alexander Scott <xander@Alexanders-Mac.local>
2014-04-29 03:51:26 -0700

    iPad Device Debug

---
+++ BeatMaker/BeatMakerTests/BeatMakerTests.m
@@ -0,0 +1,34 @@
+//
+//  BeatMakerTests.m
+//  BeatMakerTests
+//
+//  Created by Alexander Scott on 29/04/2014.
+//  Copyright (c) 2014 CADCoder. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BeatMakerTests : XCTestCase
+
+@end
+
+@implementation BeatMakerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
